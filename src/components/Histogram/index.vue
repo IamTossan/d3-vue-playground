771cc7f23510d0b43e09301fe952bede3820fbc9
iamtossan <tossanmusic@gmail.com>
2020-04-20 23:55:40 +0200

    feat(histogram): add x axis

--- src/components/Histogram/index.vue
+++ src/components/Histogram/index.vue
@@ -10,12 +10,13 @@
       :fill="svg.color"
       rx="0.5%"
     ></rect>
+    <g ref="xAxis" :transform="`translate(0, ${svg.height - svg.padding})`" />
   </svg>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
-import { scaleLinear } from 'd3';
+import { Component, Prop, Vue, Ref, Watch } from 'vue-property-decorator';
+import { scaleLinear, axisBottom, select } from 'd3';
 
 type Bar = {
   x: number;
@@ -28,33 +29,53 @@ type Bar = {
 export default class Histogram extends Vue {
   @Prop() private histogramData!: Array<number>;
 
+  @Ref('xAxis') xAxis!: SVGSVGElement;
+
   private svg = {
     height: 400,
     width: 600,
-    padding: 10,
+    padding: 20,
     color: '#4287f5',
   };
 
   get bars(): Array<Bar> {
     const XScale = scaleLinear()
       .domain([0, this.histogramData.length])
-      .range([0, this.svg.width]);
+      .range([this.svg.padding, this.svg.width - this.svg.padding]);
 
     const YScale = scaleLinear()
       .domain([0, Math.max(...this.histogramData)])
-      .range([0, this.svg.height]);
+      .range([this.svg.padding, this.svg.height - this.svg.padding * 2]);
 
     const barsData = this.histogramData.map((d, idx) => {
       const height = YScale(d);
       return {
         x: XScale(idx),
-        y: this.svg.height - height,
+        y: this.svg.height - this.svg.padding - height,
         height,
-        width: this.svg.width / this.histogramData.length - 1,
+        width:
+          (this.svg.width - this.svg.padding) / this.histogramData.length - 1,
       };
     });
 
     return barsData;
+  }
+
+  mounted() {
+    this.onHistogramDataChanged();
+  }
+
+  @Watch('histogramData')
+  onHistogramDataChanged(): void {
+    const XScale = scaleLinear()
+      .domain([0, this.histogramData.length])
+      .range([this.svg.padding, this.svg.width - this.svg.padding]);
+
+    const xAxis = axisBottom(XScale);
+
+    select(this.xAxis).call(xAxis);
+
+    return;
   }
 }
 </script>
